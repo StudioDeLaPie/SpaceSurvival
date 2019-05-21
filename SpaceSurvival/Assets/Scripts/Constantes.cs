63cb40b1794256626dc9415cfc3a3968cb43df64
Kyohei <kalops22.kikoo@gmail.com>
2019-05-21 12:10:21 +0200

    Ajout de la pression dans les dômes avec des particules

---
+++ SpaceSurvival/Assets/Scripts/Constantes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Const
+{
+    static public class Constantes
+    {
+        static public float constanteGazParfait = 8.314f;
+        static public float temperatureEnKelvin = 298.15f; //25Degré Celsius
+    }
+}
--- SpaceSurvival/Assets/Scripts/Dome.cs
+++ SpaceSurvival/Assets/Scripts/Dome.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Const;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,13 +9,67 @@ public class Dome : MonoBehaviour
     public int nbParticulesGaz = 0;
     public int nbParticulesVapeur = 0;
 
-    void Start()
+    [Header("Affichage")]
+    [SerializeField]
+    private float volume; //DemiSphère
+    [SerializeField]
+    private float pressure; //DemiSphère
+
+    private void Start()
     {
-            
+        CalculationVolume();
     }
 
-    void Update()
+    [ContextMenu("CalculationPressure")]
+    private void CalculationPressure()
     {
-        
+        pressure = ((nbParticulesOxygene + nbParticulesGaz + nbParticulesVapeur) * Constantes.constanteGazParfait * Constantes.temperatureEnKelvin) / volume;
+        Debug.Log("pression: " +pressure);
+    }
+
+    [ContextMenu("CalculationVolume")]
+    private void CalculationVolume()
+    {
+        float rayon = transform.localScale.x / 2;
+        volume = ((4 * Mathf.PI * Mathf.Pow(rayon, 3)) / 3 )/2; //mètre cube
+        CalculationPressure();
+        //Debug.Log("Volume: " + volume);
+    }
+   
+
+    public bool AddOxygene(int quantity)
+    {
+        if (nbParticulesOxygene + quantity >= 0)
+        {
+            nbParticulesOxygene += quantity;
+            CalculationPressure();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool AddGaz(int quantity)
+    {
+        if (nbParticulesGaz + quantity >= 0)
+        {
+            nbParticulesGaz += quantity;
+            CalculationPressure();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool AddVapeur(int quantity)
+    {
+        if (nbParticulesVapeur + quantity >= 0)
+        {
+            nbParticulesVapeur += quantity;
+            CalculationPressure();
+            return true;
+        }
+        else
+            return false;
     }
 }
