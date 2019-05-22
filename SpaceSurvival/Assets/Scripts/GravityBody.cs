f62b4899427c27d255212ef6fbcdb7a000a19c66
Kentarosan <jacquin.yannis@gmail.com>
2019-05-22 13:57:13 +0200

    Création Player

---
+++ SpaceSurvival/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+    public float weight = 1;
+
+    private GravityAttractor attractor;
+    private Transform myTransform;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb.useGravity = false;
+        myTransform = transform;
+        attractor = GameObject.FindGameObjectWithTag("Planete").GetComponent<GravityAttractor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(myTransform, rb, weight);
+    }
+}
