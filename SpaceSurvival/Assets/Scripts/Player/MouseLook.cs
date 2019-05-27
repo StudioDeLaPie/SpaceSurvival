44d86fd9dc515dc57c3246173938a52779915ef6
Kentarosan <jacquin.yannis@gmail.com>
2019-05-27 14:05:21 +0200

    UIPlayer + Rangement scripts

---
+++ SpaceSurvival/Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[Serializable]
+public class MouseLook
+{
+    public float XSensitivity = 2f;
+    public float YSensitivity = 2f;
+    public bool clampVerticalRotation = true;
+    public float MinimumX = -90F;
+    public float MaximumX = 90F;
+    public bool lockCursor = true;
+
+    private Quaternion _CharacterTargetRot;
+    private Quaternion _CameraTargetRot;
+    private bool _cursorIsLocked = true;
+
+    public void Init(Transform character, Transform camera)
+    {
+        _CharacterTargetRot = character.localRotation;
+        _CameraTargetRot = camera.localRotation;
+    }
+
+    public void LookRotation(Transform character, Transform camera)
+    {
+        if (_cursorIsLocked) //Tourne la caméra seulement si le cursor est lock
+        {
+
+            float yRot = Input.GetAxis("Mouse X") * XSensitivity;
+            float xRot = Input.GetAxis("Mouse Y") * YSensitivity;
+
+            _CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+
+            if (clampVerticalRotation)
+                _CameraTargetRot = ClampRotationAroundXAxis(_CameraTargetRot);
+
+            character.Rotate(character.up, yRot, Space.World);
+            camera.localRotation = _CameraTargetRot;
+
+        }
+        UpdateCursorLock();
+    }
+
+    private void UpdateCursorLock()
+    {
+        //if the user set "lockCursor" we check & properly lock the cursor
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()
+    {
+        if (_cursorIsLocked && Input.GetButtonUp("Escape"))
+        {
+            _cursorIsLocked = false;
+        }
+        else if ((Input.GetMouseButtonUp(0) && !IsPointerOverUIObject()) || Input.GetButtonUp("Cancel") || (!_cursorIsLocked && Input.GetButtonUp("Escape")))
+        {
+            _cursorIsLocked = true;
+        }
+
+        if (_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            //escapeMenu.SetActive(false);
+            //reticule.SetActive(true);
+        }
+        else if (!_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            //escapeMenu.SetActive(true);
+            //reticule.SetActive(false);
+        }
+    }
+
+    //When Touching UI
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+
+    private Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+
+}
---
+++ SpaceSurvival/Assets/Scripts/Player/PlayerRespiration.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespiration : MonoBehaviour
+{
+    public float maxOxygen;
+
+    [SerializeField] private float _oxygenConsommation;
+    private EnvironmentPlayerDetector _environmentDetector;
+    private Conteneur _conteneurEnvironnement;
+    private PlayerHealth _playerHealth;
+    private float _oxygen;
+
+    public float Oxygen { get => _oxygen; set => _oxygen = value; }
+
+    private void Start()
+    {
+        _environmentDetector = GetComponentInParent<EnvironmentPlayerDetector>();
+        _environmentDetector.OnConteneurChange += ConteneurChange;
+        _conteneurEnvironnement = _environmentDetector.EnvironmentConteneur;
+        _playerHealth = GetComponentInParent<PlayerHealth>();
+        _oxygen = maxOxygen;
+    }
+
+    private void Update()
+    {
+        ConsommeOxygen();
+        if (_conteneurEnvironnement.Pressure > 80000 && _conteneurEnvironnement.Pressure < 120000)
+        {
+            InspireOxygen();
+        }
+    }
+
+    /// <summary>
+    /// Consomme l'oxygène en stock dans la bombonne
+    /// </summary>
+    private void ConsommeOxygen()
+    {
+        float consommation = 0;
+        if (_oxygen > 0)
+        {
+            consommation = Mathf.Clamp(_oxygenConsommation * Time.deltaTime, 0, _oxygen);
+            _oxygen -= consommation;
+        }
+        else
+            _playerHealth.PerteVie(_oxygenConsommation * Time.deltaTime);
+        _conteneurEnvironnement.AddGas(EGases.Gaz, consommation);
+    }
+
+    /// <summary>
+    /// Remplit la bombonne avec l'oxygène ambiant à une vitesse proportionnelle au ratio d'oxygène
+    /// </summary>
+    private void InspireOxygen()
+    {
+        //Remplit la jauge d'oxygène si besoin
+        if (_oxygen < maxOxygen)
+        {
+            float oxygenAspire = maxOxygen * Time.deltaTime * _conteneurEnvironnement.Ratio(EGases.Oxygene);
+            oxygenAspire = Mathf.Clamp(oxygenAspire, 0, maxOxygen - _oxygen);
+            _oxygen += Mathf.Abs(_conteneurEnvironnement.AddGas(EGases.Oxygene, -oxygenAspire));
+        }
+    }
+
+    /// <summary>
+    /// Appelé par le Environment PlayerDetector lorsque le conteneur est modifié
+    /// </summary>
+    private void ConteneurChange()
+    {
+        _conteneurEnvironnement = _environmentDetector.EnvironmentConteneur;
+    }
+}
