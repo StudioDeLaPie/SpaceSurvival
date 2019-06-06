96b470059007a7067f561532f8c758437bc0ab06
Kentarosan <jacquin.yannis@gmail.com>
2019-06-06 11:39:04 +0200

    Inventaire avec items dropables + infos panel

---
+++ SpaceSurvival/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject UIPlayer;
+    public GameObject UIInventaire;
+
+    private void Start()
+    {
+        UIPlayer.SetActive(true);
+        UIInventaire.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("OuvrirInventaire"))
+        {
+            UIPlayer.SetActive(!UIPlayer.activeSelf);
+            UIInventaire.SetActive(!UIInventaire.activeSelf);
+        }
+    }
+}
---
+++ SpaceSurvival/Assets/UIDetailsItemInventaire.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIDetailsItemInventaire : MonoBehaviour
+{
+    public TextMeshProUGUI nom;
+    public TextMeshProUGUI description;
+    public Image image;
+
+    private void Start()
+    {
+        Clean();
+    }
+
+    public void AfficherInfos(Recoltable recoltable)
+    {
+        if (recoltable == null)
+            Clean();
+        else
+        {
+            nom.text = recoltable.data.nom;
+            description.text = recoltable.data.description;
+            image.sprite = recoltable.data.sprite;
+        }
+    }
+
+    public void Clean()
+    {
+        nom.text = "Nom";
+        description.text = "Description";
+        image.sprite = null;
+    }
+}
