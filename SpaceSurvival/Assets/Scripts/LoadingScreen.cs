0f1a800e1bfd7f68badf11ee423e78703ebeb464
Kentarosan <jacquin.yannis@gmail.com>
2019-06-27 10:20:44 +0200

    Fixs + écran de chargement + création scène build v0.3

---
+++ SpaceSurvival/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Slider slider;
+    public TextMeshProUGUI textProgress;
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void PercentPropsPlaced(float percent)
+    {
+        textProgress.text = "Placement aléatoire des mines";
+        slider.value = percent;
+        //Debug.Log(percent);
+    }
+}
--- SpaceSurvival/Assets/Scripts/Props/PropsSpawner.cs
+++ SpaceSurvival/Assets/Scripts/Props/PropsSpawner.cs
@@ -12,14 +12,19 @@ public class PropsSpawner : MonoBehaviour
 
     private Transform _spawnPoint;
     private Planet _planet;
+    private MinMax _planetMinMax;
+    private LoadingScreen _loadingScreen;
     private Transform _propsTransform;
     private int _nbPlaced;
+    private int _nbPropsPerIteration = 500; //Combien de spawns à faire avant de yield dans la coroutine
 
     // Start is called before the first frame update
-    public void GameStart()
+    public void GameStart(Planet planet, LoadingScreen loadingScreen)
     {
         _spawnPoint = transform;
-        _planet = GameObject.FindGameObjectWithTag("Planete").GetComponent<Planet>();
+        _planet = planet;
+        _planetMinMax = _planet.GetMinMax;
+        _loadingScreen = loadingScreen;
 
         _propsTransform = new GameObject("_Props_").transform;
 
@@ -28,10 +33,15 @@ public class PropsSpawner : MonoBehaviour
 
     private IEnumerator PlaceObjects()
     {
+        float nbPropsTotal = 0;
+        float nbPropsSpawnes = 0;
+        props.ForEach(p => nbPropsTotal += p.maxQuantity);
+
         while (props.Count > 0) //Tant qu'il reste un type de props à placer
         {
             _nbPlaced = 0;
             Prop_SO prop = props[0];
+            int nbDone = 0;
 
             for (int i = 0; i < prop.maxQuantity; i++) //Spawn de la quantité voulue de ce prop
             {
@@ -41,22 +51,23 @@ public class PropsSpawner : MonoBehaviour
 
                 GameObject obj = Instantiate(prop.prefab, _spawnPoint.position, Quaternion.identity);
                 obj.transform.parent = _propsTransform;
-                obj.GetComponent<PropsPositionneur>().propsSpawner = this;
+                obj.GetComponent<PropsPositionneur>().Placer(this, _planetMinMax);
+
+                nbDone++;
+                if (nbDone == _nbPropsPerIteration) //Si X objets ont été spawné
+                {
+                    nbPropsSpawnes += _nbPropsPerIteration;
+                    nbDone = 0;
+                    _loadingScreen.PercentPropsPlaced(nbPropsSpawnes / nbPropsTotal);
+                    yield return null; //on laisse Unity passer à la frame suivante
+                }
             }
-            while (_nbPlaced < prop.maxQuantity) //Tant qu'ils n'ont pas tous fini de se placer
-            {
-                yield return null; //on attend
-            }
+
+            nbPropsSpawnes += nbDone;
+            _loadingScreen.PercentPropsPlaced(nbPropsSpawnes / nbPropsTotal);
+
             props.Remove(prop); //Ce type de prop est placé, on le supprime de la liste
         }
         OnPropsPlaced();//On averti le GameStarter que tous les props sont placés
-    }
-
-    /// <summary>
-    /// Appelé lorsqu'un des Props instanciés touche le sol
-    /// </summary>
-    public void PropPlaced()
-    {
-        _nbPlaced++;
     }
 }
