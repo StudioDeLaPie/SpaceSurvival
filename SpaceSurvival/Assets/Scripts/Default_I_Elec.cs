49c0c0917883e4ec0b30a5fde499d596dec6e24f
Kentarosan <jacquin.yannis@gmail.com>
2019-09-10 10:12:49 +0200

    Création Repeater

---
+++ SpaceSurvival/Assets/Scripts/Default_I_Elec.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Sert simplement d'interface pour les engins électrique ne servant à rien d'autre qu'être des Batteries, Générateurs ou Consommateurs
+/// </summary>
+public class Default_I_Elec : MonoBehaviour, I_Elec
+{
+    public void TurnOff()
+    {
+    }
+
+    public void TurnOn()
+    {
+    }
+}
---
+++ SpaceSurvival/Assets/Scripts/Portables/RepeaterPortable.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeaterPortable : Portable
+{
+    public MeshRenderer meshRenderer;
+
+    public Material baseMaterial;
+    public Material rightMaterial;
+    public Material wrongMaterial;
+
+    private Collider _collider;
+
+    protected override void Start()
+    {
+        base.Start();
+        _collider = GetComponent<Collider>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (inDeplacement)
+        {
+            canBeConstruct = false;
+            meshRenderer.sharedMaterial = wrongMaterial;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (inDeplacement)
+        {
+            canBeConstruct = true;
+            meshRenderer.sharedMaterial = rightMaterial;
+        }
+    }
+
+    public override void ObjectInDeplacement()
+    {
+        _collider.isTrigger = true;
+        inDeplacement = true;
+        canBeConstruct = true;
+        meshRenderer.sharedMaterial = rightMaterial;
+        GetComponent<Connexion>().AllLinksDoUpdate(true);
+    }
+
+    public override void ObjectPlaced()
+    {
+        meshRenderer.sharedMaterial = baseMaterial;
+        inDeplacement = false;
+        _collider.isTrigger = false;
+        GetComponent<Connexion>().AllLinksDoUpdate(false);
+    }
+}
