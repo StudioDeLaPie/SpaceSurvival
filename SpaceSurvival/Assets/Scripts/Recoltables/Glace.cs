1a92fefabbbdec30c64c71be04a7063a7dfe5e0b
Kentarosan <jacquin.yannis@gmail.com>
2019-09-11 15:05:12 +0200

    Fonte des glaces

--- SpaceSurvival/Assets/Scripts/Recoltables/Glace.cs
+++ SpaceSurvival/Assets/Scripts/Recoltables/Glace.cs
@@ -1,17 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utilities;
 
 public class Glace : Recoltable
 {
+    [SerializeField] private float timeToLive;
+    [SerializeField] private Transform meshTransform;
+
     private Rigidbody _rb;
     private GravityBody _gb;
+
+    private Vector3 baseScale;
+    private float minScale = 0.3f;
+    private float droppedSince = 0;
+    private bool dropped = false;
 
     override protected void Start()
     {
         base.Start();
         _rb = GetComponent<Rigidbody>();
         _gb = GetComponent<GravityBody>();
+        dropped = gameObject.activeInHierarchy;
+        baseScale = meshTransform.localScale;
+    }
+
+    private void Update()
+    {
+        if (dropped)
+        {
+            droppedSince += Time.deltaTime;
+            if (droppedSince > timeToLive)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                float scale = Mathematiques.Map(droppedSince, 0, timeToLive, 1, minScale);
+                meshTransform.localScale = baseScale * scale;
+            }
+        }
+    }
+
+    public override Recoltable Recolte()
+    {
+        dropped = false;
+        meshTransform.localScale = baseScale;
+        return base.Recolte();
     }
 
     override public void Lacher(Vector3 dropPosition)
@@ -19,5 +54,7 @@ public class Glace : Recoltable
         base.Lacher(dropPosition);
         _rb.isKinematic = false;
         _gb.enabled = true;
+        dropped = true;
+        droppedSince = 0;
     }
 }
---
+++ SpaceSurvival/Assets/Scripts/Utilities/Mathematiques.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+    static class Mathematiques
+    {
+        public static float Map(this float value, float fromSource, float toSource, float fromTarget, float toTarget)
+        {
+            return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
+        }
+    }
+}
