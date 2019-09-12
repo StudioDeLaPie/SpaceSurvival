d7398e240bc56ae3ef6abe376cd5e067b0d0c1d5
Kentarosan <jacquin.yannis@gmail.com>
2019-09-12 09:55:12 +0200

    Panneau solaire angle de fonctionnement max

--- SpaceSurvival/Assets/Scripts/Electrique/Machines/PanneauSolaireElec.cs
+++ SpaceSurvival/Assets/Scripts/Electrique/Machines/PanneauSolaireElec.cs
@@ -6,6 +6,7 @@ public class PanneauSolaireElec : GenerateurElec
 {
     [SerializeField] private Transform panneau;
     [SerializeField, Tooltip("Angle = 0 quand soleil au dessus et 90 à l'horizon")] private float maxWorkingAngle;
+    [SerializeField, Tooltip("Angle = 0 quand soleil au dessus et 90 à l'horizon")] private float maxInclinaisonAngle;
     private Transform sun;
     private float defaultPuissance;
 
@@ -22,7 +23,8 @@ public class PanneauSolaireElec : GenerateurElec
         float angle = Vector3.Angle(-transform.up, -sun.position);
         if (angle <= maxWorkingAngle)
         {
-            panneau.forward = sun.position - panneau.position;
+            if (angle <= maxInclinaisonAngle)
+                panneau.forward = sun.position - panneau.position;
             puissance = defaultPuissance;
         }
         else
