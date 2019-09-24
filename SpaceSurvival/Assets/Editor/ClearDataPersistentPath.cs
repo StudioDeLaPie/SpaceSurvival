228f835c98ea57fc6631d2298ec9711c1636d340
Kyohei <kalops22.kikoo@gmail.com>
2019-09-24 14:29:33 +0200

    Ajout du script pour clear le data persistent path avec un shortcut

---
+++ SpaceSurvival/Assets/Editor/ClearDataPersistentPath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ClearDataPersistentPath : Editor
+{
+    [MenuItem("■ Tool/Clear Data Persistent Path %SPACE")]
+    static public void test()
+    {
+        Debug.Log("Clear Persistent Path");
+        DirectoryInfo dataDir = new DirectoryInfo(Application.persistentDataPath);
+        dataDir.Delete(true);
+    }
+}
